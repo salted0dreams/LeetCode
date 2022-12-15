6e765756b3387832905df0c58a9ddbbe1e3d294f
Aryan Sharma <96866160+salted0dreams@users.noreply.github.com>
2022-12-15 20:11:32 +0530

    Runtime Beats: 100% Memory Beats: 95.49%

--- 2433_Find_The_Original_Array_of_Prefix_Xor.cpp
+++ 2433_Find_The_Original_Array_of_Prefix_Xor.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
         ios_base::sync_with_stdio(false);
+        cin.tie(0);
         int n = pref.size();
         vector<int> ans(n);
         ans[0] = pref[0];
